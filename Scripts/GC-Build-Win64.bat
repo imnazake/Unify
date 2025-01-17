262bc77693358ded6f5597f082467920050f981e
Nazake <imnazake@gmail.com>
2025-01-17 10:25:54 +0100

    Update for UE5.4

--- Scripts/GC-Build-Win64.bat
+++ Scripts/GC-Build-Win64.bat
@@ -1,10 +1,10 @@
 @echo off
 
 set "PLUGIN_NAME=GameplayContainers"
-set "PLUGIN_PATH=C:\Users\Nazake\Documents\Unreal Projects\Demos\Unify_5.3\Plugins\GameplayContainers"
-set "UAT_PATH=D:\Program Files\Epic Games\UE_5.3\Engine\Build\BatchFiles\RunUAT.bat"
+set "PLUGIN_PATH=C:\Users\Nazake\Documents\Unreal Projects\Demos\Unify_5.4\Plugins\GameplayContainers"
+set "UAT_PATH=D:\Program Files\Epic Games\UE_5.4\Engine\Build\BatchFiles\RunUAT.bat"
 
-set "PACKAGE_FOLDER_NAME=GC_Distribution_Win64_UE5.3"
+set "PACKAGE_FOLDER_NAME=GC_Distribution_Win64_UE5.4"
 set "PACKAGE_PATH=C:\Users\Nazake\Desktop\Output"
 
 "%UAT_PATH%" BuildPlugin -Plugin="%PLUGIN_PATH%\%PLUGIN_NAME%.uplugin" -Package="%PACKAGE_PATH%\%PACKAGE_FOLDER_NAME%" -Rocket -VS2022
