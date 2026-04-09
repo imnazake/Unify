667fb65a62f900c15db26b43aff246ce064ada0d
imneoxyne <imneoxyne@gmail.com>
2026-04-09 15:13:54 +0100

    Refactor interaction test setup and add level actors for interaction/policy testing

--- Scripts/GI-Build-Win64.bat
+++ Scripts/GI-Build-Win64.bat
@@ -1,8 +1,8 @@
 @echo off
 
 set "PLUGIN_NAME=GameplayInteraction"
-set "PLUGIN_PATH=C:\Users\Nazake\Documents\Unreal Projects\Demos\Unify_5.5\Plugins\GameplayInteraction"
-set "UAT_PATH=D:\Program Files\Epic Games\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat"
+set "PLUGIN_PATH=D:\Projects\Unify-GI-UE5.5-Dev\Plugins\GameplayInteraction"
+set "UAT_PATH=D:\Epic Games\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat"
 
 set "PACKAGE_FOLDER_NAME=GI_Distribution_Win64_UE5.5"
 set "PACKAGE_PATH=C:\Users\Nazake\Desktop\Output"
